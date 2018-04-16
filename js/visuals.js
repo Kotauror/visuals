e2386685e17c6b05a972f133e252ffbf77a37102
kotpsot <zygmunt.justyna@gmail.com>
2018-04-16 13:57:47 +0100

    Add form to main page.

--- js/visuals.js
+++ js/visuals.js
@@ -5,15 +5,27 @@ $(document).ready(() => {
     .attr("height", "100%")
     .attr('class', 'svg')
 
+  $('.data_form').submit(function(event) {
+      event.preventDefault()
+      let x1 = $( ".x1" ).val();
+      let x2 = $( ".x2" ).val();
+      let y1 = $( ".y1" ).val();
+      let y2 = $( ".y2" ).val();
+      let iterator = $( ".iterator" ).val();
+      draw(x1, x2, y1, y2, iterator)
+  })
 
-  var i = 20;
-  for (;i < 700;) {
-  canvas.append("line")
-    .attr('x1', 20)
-    .attr('y1', 20)
-    .attr('x2', 90)
-    .attr('y2', i)
-    .style('stroke', '#3e3330')
-    i += 20
+  let draw = function(x1, x2, y1, y2, iterator) {
+    var i = iterator;
+    for (;i < 700;) {
+    canvas.append("line")
+      .attr('x1', x1)
+      .attr('y1', y1)
+      .attr('x2', x2 + iterator)
+      .attr('y2', y2)
+      .style('stroke', '#3e3330')
+      i += iterator
+    }
   }
+
 })
