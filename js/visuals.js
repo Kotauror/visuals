82f93d3aaa42cb132c2edd62a89f59ef1c5c44a0
kotpsot <zygmunt.justyna@gmail.com>
2018-04-16 15:02:18 +0100

    Add new features to the form.

--- js/visuals.js
+++ js/visuals.js
@@ -12,19 +12,19 @@ $(document).ready(() => {
       let y1 = $( ".y1" ).val();
       let y2 = $( ".y2" ).val();
       let iterator = $( ".iterator" ).val();
-      draw(x1, x2, y1, y2, iterator)
+      let limit = $( ".limit" ).val();
+      draw(x1, x2, y1, y2, iterator, limit)
   })
 
-  let draw = function(x1, x2, y1, y2, iterator) {
-    var i = iterator;
-    for (;i < 700;) {
+  let draw = function(x1, x2, y1, y2, iterator, limit) {
+    for (var i = 2;i < (parseInt(limit));) {
     canvas.append("line")
-      .attr('x1', x1)
-      .attr('y1', y1)
-      .attr('x2', x2 + iterator)
-      .attr('y2', y2)
+      .attr('x1', (parseInt(x1) + i))
+      .attr('y1', (parseInt(y2)))
+      .attr('x2', (parseInt(x2)))
+      .attr('y2', (parseInt(y2) + i))
       .style('stroke', '#3e3330')
-      i += iterator
+      i += (parseInt(iterator))
     }
   }
 
