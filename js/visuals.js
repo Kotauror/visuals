88aed223506b86c89be4b476b0a34be868bd0ba3
kotpsot <zygmunt.justyna@gmail.com>
2018-04-16 15:54:01 +0100

    Form ready.

--- js/visuals.js
+++ js/visuals.js
@@ -13,19 +13,33 @@ $(document).ready(() => {
       let y2 = $( ".y2" ).val();
       let iterator = $( ".iterator" ).val();
       let limit = $( ".limit" ).val();
-      draw(x1, x2, y1, y2, iterator, limit)
+      let strokeCol = $( ".stroke-col" ).val();
+      draw(x1, x2, y1, y2, iterator, limit, strokeCol)
   })
 
-  let draw = function(x1, x2, y1, y2, iterator, limit) {
-    for (var i = 2;i < (parseInt(limit));) {
+  // $('.data_form_bg').submit(function(event) {
+  //     event.preventDefault()
+  //     let bgCol = $( ".bg-col" ).val();
+  //     changeBackground(bgCol)
+  // })
+
+  let draw = function(x1, x2, y1, y2, iterator, limit, strokeCol) {
+    for (let i = 2;i < (parseInt(limit));) {
     canvas.append("line")
       .attr('x1', (parseInt(x1) + i))
       .attr('y1', (parseInt(y2)))
       .attr('x2', (parseInt(x2)))
       .attr('y2', (parseInt(y2) + i))
-      .style('stroke', '#3e3330')
+      .style('stroke', strokeCol)
       i += (parseInt(iterator))
     }
   }
+
+  // let changeBackground = function(bgCol) {
+  //   var chart = canvas.append("canvas")
+  //     .attr("fill", "pink");
+  //
+  // }
+
 
 })
